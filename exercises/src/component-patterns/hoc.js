934da09eaf3c8a647ff9bfa636a63380d4adcbec
rvgpl <kesariravigopal@gmail.com>
2018-04-01 09:11:50 +0530

    Adds HOC exercise solution

--- exercises/src/component-patterns/hoc.js
+++ exercises/src/component-patterns/hoc.js
@@ -10,12 +10,32 @@ const getDisplayName = WrappedComponent => {
 /**
  * HOC
  */
-const logProps = WrappedComponent => {};
+const logProps = WrappedComponent => {
+  class WithLog extends React.Component {
+    componentDidUpdate = prevProps => {
+      console.log("Previous props", prevProps);
+      console.log("Current props", this.props);
+    };
+
+    render() {
+      return <WrappedComponent {...this.props} />
+    }
+  }
+
+  WithLog.displayName = `LogProps(${getDisplayName(WrappedComponent)}`;
+
+  return WithLog;
+};
 
 /**
  * Display current count from props
  */
-class ChildComponent extends React.Component {}
+class ChildComponent extends React.Component {
+
+  render() {
+    return <div>{this.props.count}</div>
+  }
+}
 
 /**
  * Enhance the child Component
@@ -26,6 +46,25 @@ const WithLog = logProps(ChildComponent);
  * A React class with counter, which increments on press of a button.
  * It passes the current count to its child
  */
-class Counter extends React.Component {}
+class Counter extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      count: 0
+    };
+  }
+  increment = () => {
+    this.setState(prevState => ({count: prevState.count + 1}));
+  }
+
+  render() {
+    return (
+      <div>
+        <button onClick={this.increment}>+</button>
+        <WithLog count={this.state.count} />
+      </div>
+    );
+  }
+}
 
 export default Counter;
