21513e13e4f312a38095838208330b627f61d7f1
rvgpl <kesariravigopal@gmail.com>
2018-04-01 10:28:47 +0530

    Adds solution for jest exercise 4

--- exercises/src/testing-react-component/components/Counter/Exercise4.test.js
+++ exercises/src/testing-react-component/components/Counter/Exercise4.test.js
@@ -2,19 +2,17 @@ import React from "react";
 import { shallow } from "enzyme";
 import Counter from ".";
 
-// Exasice #4
+// Exercise #4
 describe("User interactions on counter", () => {
 
   it("increment the count when the increment button is clicked", () => {
     const wrapper = shallow(<Counter />);
-    // ...
     wrapper.find('Button.increment').simulate('click');
     expect(wrapper.find(".count").text()).toEqual("1");
   });
 
   it("decrement the count when the decrement button is clicked", () => {
     const wrapper = shallow(<Counter />);
-    // ...
     wrapper.find('Button.decrement').simulate('click');
     expect(wrapper.find(".count").text()).toEqual("-1");
   });
